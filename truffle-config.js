66bb1109db7beadda83d45f8fef0f9536c80d31e
efengcf <evan@coinfund.io>
2021-11-14 22:19:29 -0500

    staging env test

--- truffle-config.js
+++ truffle-config.js
@@ -1,3 +1,8 @@
+const HDWalletProvider = require('@truffle/hdwallet-provider');
+const dotenv = require('dotenv');
+dotenv.config();
+const mnemonic = process.env.MNEMONIC;
+
 module.exports = {
   networks: {
     local: {
@@ -6,7 +11,7 @@ module.exports = {
       network_id: "*" // Match any network id
     }
     ropsten: {
-      provider: () => new HDWalletProvider(mnemonic, `https://mainnet.infura.io/v3/5602dc569e764b9f9bc1e97a1ed1bcb4`),
+      provider: () => new HDWalletProvider(mnemonic, `https://ropsten.infura.io/v3/5602dc569e764b9f9bc1e97a1ed1bcb4`),
       network_id: 3,       // Ropsten's id
       gas: 5500000,        // Ropsten has a lower block limit than mainnet
       confirmations: 2,    // # of confs to wait between deployments. (default: 0)
